52bac85156508edccc4d30272f82b59eda7f6d8d
HenriqueNas <henriquenas.dev@gmail.com>
2023-12-24 17:37:29 -0300

    finish `/status` endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,15 +1,55 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
 import database from "@infra/database";
 
+interface StatusResponse {
+  env: typeof process.env.NODE_ENV;
+  updated_at: string;
+  database: DatabaseStatusResponse;
+}
+
+interface DatabaseStatusResponse {
+  status: "helthly" | "unhealthy";
+  max_connections: number;
+  opened_connections: number;
+  version: string;
+}
+
 async function status(
-  request: NextApiRequest,
+  _: NextApiRequest,
   response: NextApiResponse,
 ): Promise<void> {
-  const result = await database.query("SELECT 1 + 1;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const queryServerVersion = await database.query("SHOW server_version;");
+  const serverVersionResult = queryServerVersion.rows[0].server_version;
 
-  response.status(200).json({ status: "OK" });
+  const queryMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnextionsResult = queryMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const queryOpenedConnections = await database.query({
+    text: `SELECT count(*)::int 
+       FROM pg_stat_activity
+      WHERE datname = $1
+        AND state = 'active';`,
+    values: [databaseName],
+  });
+  const openedConnectionsResult = queryOpenedConnections.rows[0].count;
+
+  const databaseStatus: DatabaseStatusResponse = {
+    status: "helthly",
+    opened_connections: Number(openedConnectionsResult),
+    max_connections: Number(maxConnextionsResult),
+    version: serverVersionResult.toString(),
+  };
+
+  response.status(200).json({
+    env: process.env.NODE_ENV,
+    updated_at: updatedAt,
+    database: databaseStatus,
+  });
 }
 
 export default status;
+export type { StatusResponse, DatabaseStatusResponse };
--- tests/integrations/api/v1/status/get.test.ts
+++ tests/integrations/api/v1/status/get.test.ts
@@ -1,7 +1,55 @@
-import { expect, test } from "bun:test";
+import { DatabaseStatusResponse, StatusResponse } from "@v1/status";
+import { beforeAll, expect, test, describe } from "bun:test";
 
-test("GET /api/v1/status should return status 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET /api/v1/status", () => {
+  var response: Response;
+  var body: StatusResponse;
 
-  expect(response.status).toEqual(200);
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    body = await response.json();
+  });
+
+  test("should return status 200", () => {
+    expect(response.status).toEqual(200);
+  });
+
+  test("should return valid updated_at as ISO string", () => {
+    expect(body.updated_at).toBeDefined();
+
+    const parsedUpdatedAt = new Date(body.updated_at).toISOString();
+    expect(parsedUpdatedAt).toEqual(body.updated_at);
+  });
+});
+
+describe("GET /api/v1/status database", () => {
+  var response: Response;
+  var database: DatabaseStatusResponse;
+
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    database = (await response.json()).database;
+  });
+
+  test("database status should be healtly", () => {
+    expect(database.status).toBeDefined();
+    expect(database.status).toEqual("helthly");
+  });
+
+  test("should return a valid current database version", () => {
+    expect(database.version).toBeDefined();
+    expect(database.version).toBeTypeOf("string");
+    expect(database.version).toEqual("16.0");
+  });
+
+  test("should return the current opened connections as number", () => {
+    expect(database.opened_connections).toBeDefined();
+    expect(database.opened_connections).toBeTypeOf("number");
+    expect(database.opened_connections).toEqual(1);
+  });
+
+  test("should return the max connections for database", () => {
+    expect(database.max_connections).toBeDefined();
+    expect(database.max_connections).toBeTypeOf("number");
+  });
 });
